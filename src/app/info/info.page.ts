8f0b6a105bca1808edad10f81edb6beb492d47c9
LordMixy <diegoalongi4@gmail.com>
2022-03-18 11:56:00 +0100

    AAAAAAAAAAAAAAAAA

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 
 @Component({
@@ -10,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
 export class InfoPage implements OnInit {
   public nickname: string;
   public isGuest: boolean;
+  public imageUrl: string;
 
   constructor(private Router : Router) { 
+    this.imageUrl = '../../assets/user.svg';
     this.nickname = localStorage.getItem('nickname') ? localStorage.getItem('nickname') : 'Accedi o registrati per avere un nickname'; 
     this.isGuest = localStorage.getItem('guest') === 'true';
+    /*
+    axios.get('https://api.thecatapi.com/v1/images/search').then(res => {
+      this.imageUrl = res.data[0].url;
+    });
+    */
   }
 
   ngOnInit() {
